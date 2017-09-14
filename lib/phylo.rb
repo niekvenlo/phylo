6eafa3047d6c99e8d530f3166ac479852c3f19dc
Nick Beaujean <niekvenlo@gmail.com>
2017-09-14 15:16:34 +0000

    init Gem with basic tree code

---
+++ lib/phylo.rb
@@ -0,0 +1,15 @@
+require "phylo/version"
+
+def phylo(depth = 0, path = nil)
+  this_dir = `ls -F #{path}`
+  this_dir.split.each do |f|
+    if f.end_with? "/"
+      puts "#{"| "*depth}\e[95m#{f}\e[0m"
+      new_path = "#{path}#{f}"
+      phylo(depth + 1, new_path)
+    else
+      puts "#{"| "*depth}#{f}"
+    end
+  end
+end
+phylo
---
+++ lib/phylo/version.rb
@@ -0,0 +1,3 @@
+module Phylo
+  VERSION = "0.1.0"
+end
